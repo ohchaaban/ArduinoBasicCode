191e1cf823bcb16829ba68905f7825d9349c9173
o_chaaban <o.chaaban.projects@gmail.com>
2018-02-04 21:23:35 -0600

    function added

--- BlinkDelayFunction/BlinkDelayFunction.ino
+++ BlinkDelayFunction/BlinkDelayFunction.ino
@@ -1,12 +1,12 @@
 /*
   Blink with delay function.   
   
-  Blink Example modified 04 Feb 2018
+  modified 04 Feb 2018
   by Omar Chaaban
  */
 
 const int LedPin = 7;         //Pin connected to the led
-const long delayPin = 1000;   //Delay's led in milliseconds
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -17,9 +17,14 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   
-  digitalWrite(LedPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(delayPin);              // wait for a second
-  digitalWrite(LedPin, LOW);    // turn the LED off by making the voltage LOW
-  delay(delayPin);
-  
+blinkDelay(LedPin, 2000,1000); //Turn On the ledPin for two sec, turn off the led for one sec.
 }
+
+// BlinkDelay Fuction
+void blinkDelay(int Pin, long timeHigh, long timeLow){
+  digitalWrite(Pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(timeHigh);              // wait for a second
+  digitalWrite(Pin, LOW);    // turn the LED off by making the voltage LOW
+  delay(timeLow);
+}
+
